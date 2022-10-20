f4e37a7fb69be8f6f044fafe608b081ba3ad556a
Arthur Matassoli <arthurmatassoli@gmail.com>
2022-10-20 18:15:40 -0300

    foi

---
+++ python/exercicios/ex004.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela.
+
+n = input("Digite algo: ")
+print(type(n))
+print("O objeto é um número? {}".format(n.isnumeric()))
+print("O objeto é alphanúmerico? {}".format(n.isalnum()))
+print("O objeto está todo em letras minúsculas? {}".format(n.islower()))
+print("O objeto está todo em letras maiúsculas? {}".format(n.isupper()))
+print("O objeto é um número decimal? {}".format(n.isdecimal()))
+print("O objeto é um espaço? {}".format(n.isspace()))
---
+++ python/teste1.py
@@ -0,0 +1,19 @@
+print("Olá, Mundo!")
+print(7 + 4) = 11
+print("7" + "4") = 74
+print("Olá", 5) = Olá 5
+
+=========================
+
+nome = Arthur
+idade = 19
+peso = 100
+
+print(nome, idade, peso) = Arthur 19 100
+
+nome = imput("Qual seu nome?")
+idade = imput("Qual sua idade?")
+peso = imput("Quantos anos voçê tem?")
+
+print(nome, idade, peso)
+
